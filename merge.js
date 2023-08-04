56888b1339f7c69c64d55016340380eeeb67bce2
Binyameen <bmghafoor@gmail.com>
2023-08-04 11:08:53 -0400

    merge

--- merge.js
+++ merge.js
@@ -1,5 +1,27 @@
-function merge() {}
+function merge(arr1, arr2) {
+  const results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
